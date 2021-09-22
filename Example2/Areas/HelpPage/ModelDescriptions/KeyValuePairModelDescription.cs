7380457dec8a204fa86d976b3abc027d70bf1f02
patel <patel@DESKTOP-UNHG1NL>
2021-09-22 13:01:57 -0400

    Add project files.

---
+++ Example2/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Example2.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
