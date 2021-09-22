9abe5f14c11cf6c9b4ffb8d8d7dbaaf9e11c72ec
Hardi Hemantkumar Patel <patelhardi5@gmail.com>
2021-09-22 13:03:59 -0400

    add controller

---
+++ Example2/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Example2.Controllers
+{
+    public class TestController : ApiController
+    {
+    }
+}
